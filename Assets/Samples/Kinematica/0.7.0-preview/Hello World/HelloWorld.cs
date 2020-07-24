f63f8386c204ed4aa7ea18c5fbd3ceb51d07eb66
Devitt Upkins <evilotaku@gmail.com>
2020-07-24 15:11:26 -0500

    initial commit

---
+++ Assets/Samples/Kinematica/0.7.0-preview/Hello World/HelloWorld.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Kinematica;
+using UnityEngine;
+
+namespace HelloWorld
+{
+    [RequireComponent(typeof(Kinematica))]
+    public class HelloWorld : MonoBehaviour
+    {
+        bool idle;
+
+        void Update()
+        {
+            if (Input.anyKeyDown)
+            {
+                idle ^= true;
+
+                var kinematica = GetComponent<Kinematica>();
+
+                ref var synthesizer = ref kinematica.Synthesizer.Ref;
+
+                if (idle)
+                {
+                    synthesizer.Root.Action().PlayFirstSequence(
+                        synthesizer.Query.Where(
+                            Locomotion.Default).And(Idle.Default));
+                }
+                else
+                {
+                    synthesizer.Root.Action().PlayFirstSequence(
+                        synthesizer.Query.Where(
+                            Locomotion.Default).Except(Idle.Default));
+                }
+            }
+        }
+    }
+}
